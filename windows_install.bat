5ae13e2c02ba5ccd92ceaacbd5242077452080c5
Zephyr <willtien@outlook.com>
2026-05-08 17:54:19 +0800

    Update windows_install.bat to improve PowerShell execution

--- windows_install.bat
+++ windows_install.bat
@@ -1,5 +1,10 @@
 @echo off
-chcp 65001 >nul
-:: 同目录调用ps1，绕过PowerShell执行策略
-powershell -ExecutionPolicy Bypass -File "%~dp0windows_install.ps1"
+title 正在启动 PowerShell 安装...
+mode con cols=80 lines=10
+cd /d "%~dp0"
+
+:: 强制用 PowerShell 运行（不会进入 cmd）
+powershell.exe -NoLogo -ExecutionPolicy Bypass -File "%~dp0windows_install.ps1"
+
 pause
+exit
